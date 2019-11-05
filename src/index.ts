8ce585e50e3a5d3a9f11f427394e1b62c4456b0f
Vít Rozsíval <mail@vitrozsival.cz>
2019-11-05 15:04:46 +0100

    Add loader and runtime generator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import SpritesConstantsGeneratorPlugin from './SpritesConstantsGeneratorPlugin';
 
+export const loader = (source: string): string =>
+  `export default ${source
+    .split('\n')
+    .filter(svg => svg !== '')
+    .map(svg => svg.replace(/fill="#[a-z0-9]{3,6}"/g, 'fill="currentColor"'))
+    .join('\n')}`;
+
+export const runtimeGenerator = ({
+  symbol,
+}: {
+  symbol: { request: { file: string } };
+}): string =>
+  `export default __webpack_public_path__ + ${JSON.stringify(
+    symbol.request.file,
+  )}`;
+
 export default SpritesConstantsGeneratorPlugin;
