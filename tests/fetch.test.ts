856571ae63dae6d0597c9247c5fb25593c6d3150
Vít Rozsíval <mail@vitrozsival.cz>
2019-11-06 13:47:28 +0100

    Update request mock

--- tests/fetch.test.ts
+++ tests/fetch.test.ts
@@ -5,21 +5,25 @@ import fetch from '../src/SpritesConstantsGeneratorPlugin/fetch';
 describe('fetch', () => {
   afterEach(() => nock.cleanAll());
   describe('success', () => {
-    nock('http://localhost')
-      .get('/test-url')
-      .reply(200, '<svg></svg>');
-    it('returns fetched SVG', async () => {
-      await expect(fetch('http://localhost/test-url')).resolves.toBe(
-        '<svg></svg>',
-      );
+    beforeEach(() => {
+      nock('http://somedomain.dev')
+        .get('/test-url')
+        .reply(200, '<svg></svg>');
+      it('returns fetched SVG', async () => {
+        await expect(fetch('http://somedomain.dev/test-url')).resolves.toBe(
+          '<svg></svg>',
+        );
+      });
     });
   });
   describe('error', () => {
-    nock('http://localhost')
-      .get('/test-url')
-      .reply(500);
+    beforeEach(() => {
+      nock('http://somedomain.dev')
+        .get('/test-url')
+        .reply(500);
+    });
     it('returns error message', async () => {
-      await expect(fetch('http://localhost/test-url')).rejects.toThrow(
+      await expect(fetch('http://somedomain.dev/test-url')).rejects.toThrow(
         'Unable to fetch sprite contents from webpack-dev-server.',
       );
     });
