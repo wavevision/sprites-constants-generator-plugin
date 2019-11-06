935b24e0dfe725d797c4d35cf6d530ab9fc82d75
Vít Rozsíval <mail@vitrozsival.cz>
2019-11-06 18:04:50 +0100

    Update log header color

--- src/SpritesConstantsGeneratorPlugin/utils.ts
+++ src/SpritesConstantsGeneratorPlugin/utils.ts
@@ -3,7 +3,7 @@ import chalk from 'chalk';
 
 export const logStart = (icon: string, message: string): void => {
   console.log('');
-  console.log(icon, chalk.blue.bold.underline(message));
+  console.log(icon, chalk.yellowBright.bold.underline(message));
   console.log('');
 };
 
