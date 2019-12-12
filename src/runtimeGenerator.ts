26dcb4011ff77d38bc1c9ffa7e039cf2f0138ced
Vít Rozsíval <mail@vitrozsival.cz>
2019-12-12 22:48:39 +0100

    Fix runtime generator output path

--- src/runtimeGenerator.ts
+++ src/runtimeGenerator.ts
@@ -3,6 +3,6 @@ type RuntimeGenerator = {
 };
 
 const runtimeGenerator = ({ symbol }: RuntimeGenerator): string =>
-  `export default __webpack_public_path__ + ${JSON.stringify(symbol.id)}`;
+  `export default ${JSON.stringify(symbol.id)}`;
 
 export = runtimeGenerator;
--- tests/runtimeGenerator.test.ts
+++ tests/runtimeGenerator.test.ts
@@ -3,7 +3,7 @@ import runtimeGenerator from '../src/runtimeGenerator';
 describe('runtimeGenerator', () => {
   it('return symbol export', () => {
     expect(runtimeGenerator({ symbol: { id: 'symbol-id' } })).toBe(
-      'export default __webpack_public_path__ + "symbol-id"',
+      'export default "symbol-id"',
     );
   });
 });
